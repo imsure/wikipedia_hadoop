75fd8e70000c44f10569ceaec5b08ee0db485e1d
Shuo Yang <imsure95@gmail.com>
2013-10-31 00:28:18 -0500

    Adding run.sh

---
+++ run.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+hadoop fs -rm -r enwiki.invertedindex
+
+hadoop jar wikipedia-1.0.jar seis736.wikipedia.WikipediaDriver -libjars /home/shuo/Cloud9/dist/cloud9-1.4.17.jar,/home/shuo/Cloud9/lib/guava-14.0-rc3.jar,/home/shuo/Cloud9/lib/fastutil-6.5.4.jar,/home/shuo/Cloud9/lib/bliki-core-3.0.16.jar
+
